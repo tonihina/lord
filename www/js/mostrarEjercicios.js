14b2ebb83be9c4e572f617686aad99525c3726f6
Tonihina <naruhinabella@gmail.com>
2020-02-11 19:05:30 -0600

    lordF

---
+++ www/js/mostrarEjercicios.js
@@ -0,0 +1,51 @@
+$(document).ready(function() {
+    $.ajax({
+            type: "POST",
+            url: "https://andreaconeche.000webhostapp.com/MostrarEjerciciosPrincipiantes.php",
+            cache: false,
+            success: function(response)
+            {
+                $('#principiantes').html(response).fadeIn();
+            }
+    });
+
+});
+
+$(document).ready(function() {
+    $.ajax({
+            type: "POST",
+            url: "https://andreaconeche.000webhostapp.com/mostrarEjerciciosIntermedios.php",
+            cache: false,
+            success: function(response)
+            {
+                $('#intermedios').html(response).fadeIn();
+            }
+    });
+
+});
+
+$(document).ready(function() {
+    $.ajax({
+            type: "POST",
+            url: "https://andreaconeche.000webhostapp.com/mostrarEjerciciosAvanzados.php",
+            cache: false,
+            success: function(response)
+            {
+                $('#avanzados').html(response).fadeIn();
+            }
+    });
+
+});
+
+$(document).ready(function() {
+    $.ajax({
+            type: "POST",
+            url: "https://andreaconeche.000webhostapp.com/mostrarEjerciciosMuyPrincipiante.php",
+            cache: false,
+            success: function(response)
+            {
+                $('#muyPrincipiantes').html(response).fadeIn();
+            }
+    });
+
+});
